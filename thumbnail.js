0a806b0f213ae7fa8ca5b4adde218fdef269a55c
Alexander-lu <102196935+Alexander-lu@users.noreply.github.com>
2022-03-23 15:36:13 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
 });
+
+window.onload=function(){
+let targetImage = document.querySelector("#smart-image");
+  
+
+
+
+targetImage.addEventListener("click", function() {
+if (targetImage.classList.contains("small")) {
+targetImage.classList.remove("small");
+} else {
+targetImage.classList.add("small");
+}
+});
+}
